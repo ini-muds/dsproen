0ce1aefe4b61a2bcd924aa7e44a3dfe97d121d96
ini_muds <s2322007@stu.musashino-u.ac.jp>
2024-01-15 17:55:21 +0900

    再調整

--- data/scra.py
+++ data/scra.py
@@ -1,13 +1,12 @@
 import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from time import sleep
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import Select
 from bs4 import BeautifulSoup
 
 def save_to_csv(filename, products):
-    # CSVファイルを開いて商品名を書き込む
     with open(filename, mode='a', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         for product in products:
@@ -20,16 +19,19 @@ def amazon_get(search_keyword):
     driver = webdriver.Chrome(service=service, options=options)
 
     driver.get('https://www.amazon.co.jp/')
-    driver.implicitly_wait(5)
-    sleep(2)
 
-    search_input = driver.find_element(By.CSS_SELECTOR, 'div.nav-search-field > input')
+    # 検索入力が表示されるのを待つ
+    wait = WebDriverWait(driver, 10)
+    search_input = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[name="field-keywords"]')))
+
     search_input.send_keys(search_keyword)
-    driver.find_element(By.CSS_SELECTOR, 'div.nav-right > div > span > input').click()
-    sleep(10)  # 検索結果が表示されるまで待つ
-    
+    search_input.submit()
+
+    # 検索結果が読み込まれるのを待つ
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '[data-component-type="s-search-result"]')))
+
     page_source = driver.page_source
-    soup = sBeautifulSoup(page_source, 'html.parser')
+    soup = BeautifulSoup(page_source, 'html.parser')
     product_elements = soup.find_all('span', class_='a-size-base-plus a-color-base a-text-normal')
     product_names = [elem.get_text() for elem in product_elements[:20]]  # 上位20商品の名前を取得
 
